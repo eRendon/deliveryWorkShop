e878f0be87c23fffb37d966da4479bd98c86b2c1
eRendonC <erendoncc@gmail.com>
2020-10-08 17:57:08 -0500

    create proyect
    add material ionic
    create base proyect
    add login form

---
+++ src/app/amd.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table'
+import { MatStepperModule } from '@angular/material/stepper'
+import { MatButtonModule } from '@angular/material/button'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatOptionModule } from '@angular/material/core'
+import { MatSelectModule } from '@angular/material/select'
+import { MatIconModule } from '@angular/material/icon'
+import { MatPaginatorModule } from '@angular/material/paginator'
+import { MatSortModule } from '@angular/material/sort'
+import { MatGridListModule } from '@angular/material/grid-list'
+import { MatListModule } from '@angular/material/list'
+import { MatDividerModule } from "@angular/material/divider";
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    MatTableModule,
+    MatStepperModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatIconModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatGridListModule,
+    MatListModule,
+    MatDividerModule
+  ]
+})
+export class AMDModule { }
