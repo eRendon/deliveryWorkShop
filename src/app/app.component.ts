80786891d6a19548176af19000d4795bf8e2b122
eRendonC <erendoncc@gmail.com>
2020-10-09 13:31:18 -0500

    add firebase plugins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import * as firebase from 'firebase/app';
+import { firebaseConfig } from './credentials';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -22,6 +25,7 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      window.firebase.initializeApp(firebaseConfig)
     });
   }
 }
